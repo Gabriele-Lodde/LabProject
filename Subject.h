f4ab3ee05478d2f9dc5d1b720185c757e3db3709
Gabriele <gabrilodde@gmail.com>
2025-12-05 16:04:41 +0100

    Add class Subject.h for Observer pattern

--- Subject.h
+++ Subject.h
@@ -1,8 +1,17 @@
-//
-// Created by gabriele on 12/5/25.
-//
-
 #ifndef SUBJECT_H
 #define SUBJECT_H
+#include <list>
+
+#include "Observer.h"
+
+class Subject {
+public:
+    virtual ~Subject() = default;
+    virtual void addObserver(Observer* o) = 0;
+    virtual void removeObserver(Observer* o) = 0;
+    virtual void notifyAll() = 0;
+protected:
+    std::list <Observer*> observers;
+};
 
 #endif //SUBJECT_H
