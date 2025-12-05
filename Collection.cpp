bf795e7b1ec3279a740897828878b4d0cc34d751
Gabriele <gabrilodde@gmail.com>
2025-12-05 18:14:58 +0100

    Change removeNote()

--- Collection.cpp
+++ Collection.cpp
@@ -7,11 +7,11 @@ std::string Collection::getName() const {
     return name;
 }
 
-int Collection::size() const {
+int Collection::getSize() const {
     return notes.size();
 }
 
-bool Collection::addNote(std::shared_ptr<Note> note) {
+bool Collection::addNote(const std::shared_ptr<Note>& note) {
     if (note != nullptr) {
         notes.push_back(note);
         notifyAll();
@@ -20,7 +20,7 @@ bool Collection::addNote(std::shared_ptr<Note> note) {
     return false;
 }
 
-bool Collection::removeNote(std::shared_ptr<Note> note) {
+bool Collection::removeNote(const std::shared_ptr<Note>& note) {
     auto it=std::find(notes.begin(), notes.end(), note);
     if (it == notes.end())
         return false;
