81fab35d01b67a384b16f4cd40ff7951e27ebc19
Gabriele <gabrilodde@gmail.com>
2025-12-05 16:47:53 +0100

    Add class Note.cpp with methods definitions

--- Note.cpp
+++ Note.cpp
@@ -1,5 +1,41 @@
-//
-// Created by gabriele on 12/5/25.
-//
-
 #include "Note.h"
+
+Note::Note(const std::string &title, const std::string &text, bool locked, bool important) : title(title), text(text), locked(locked), important(important) {}
+
+std::string Note::getTitle() const {
+    return title;
+}
+
+std::string Note::getText() const {
+    return text;
+}
+
+bool Note::isLocked() const {
+    return locked;
+}
+
+bool Note::isImportant() const {
+    return important;
+}
+
+void Note::setLocked(bool newLocked) {
+    locked = newLocked;
+}
+
+void Note::setImportant(bool newImportant) {
+    important = newImportant;
+}
+
+bool Note::setTitle(const std::string &newTitle) {
+    if (locked)
+        return false;
+    title = newTitle;
+    return true;
+}
+
+bool Note::setText(const std::string &newText) {
+    if (locked)
+        return false;
+    text = newText;
+    return true;
+}
