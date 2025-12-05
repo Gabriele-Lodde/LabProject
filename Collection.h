27acd2fea84969bb3a382bf00301fa4f607c2458
Gabriele <gabrilodde@gmail.com>
2025-12-05 16:42:44 +0100

    Add classes Note.h and Collection.h with attributes and method declarations

--- Collection.h
+++ Collection.h
@@ -1,14 +1,31 @@
-//
-// Created by gabriele on 12/5/25.
-//
-
 #ifndef COLLECTION_H
 #define COLLECTION_H
+#include <memory>
+#include <string>
+
+#include "Note.h"
+#include "Subject.h"
 
 
+class Collection : public Subject {
+public:
+    //Constructor
+    explicit Collection(const std::string& n) : Collection(n) {}
 
-class Collection {
+    //Collection methods
+    std::string getName() const;
+    int size() const;
+    void addNote(std::shared_ptr<Note> note);
+    void removeNote(std::shared_ptr<Note> note);
 
+    //Subject interface
+    void addObserver(Observer* o) override;
+    void removeObserver(Observer* o) override;
+    void notifyAll() override;
+
+private:
+    std::string name;
+    std::list<std::shared_ptr<Note>> notes;
 };
 
 
--- Note.h
+++ Note.h
@@ -1,16 +1,32 @@
-//
-// Created by gabriele on 12/5/25.
-//
-
 #ifndef NOTE_H
 #define NOTE_H
-
+#include <string>
 
 
 class Note {
+public:
+    //Constructor
+    Note(const std::string& title, const std::string& text, bool locked, bool important) : title(title), text(text), locked(locked), important(important) {}
 
+    //Getter
+    std::string getTitle() const;
+    std::string getText() const;
+    bool isLocked() const;
+    bool isImportant() const;
+
+    //Setter of locked and important
+    void setLocked(bool newLocked);
+    void setImportant(bool newImportant);
+
+    //Setter of title and text (they return false if the note is blocked)
+    bool setTitle(const std::string& newTitle);
+    bool setText(const std::string& newText);
+
+private:
+    std::string title;
+    std::string text;
+    bool locked{false};
+    bool important{false};
 };
-
-
 
 #endif //NOTE_H
