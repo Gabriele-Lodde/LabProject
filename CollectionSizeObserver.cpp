0d2a182e9cd3445a9d9ac1f9bf8ae5338d65fa2d
Gabriele <gabrilodde@gmail.com>
2025-12-07 01:38:38 +0100

    Remove getLastSize() method, change update() method, remove destructor

--- CollectionSizeObserver.cpp
+++ CollectionSizeObserver.cpp
@@ -1,17 +1,12 @@
 #include "CollectionSizeObserver.h"
 
-CollectionSizeObserver::CollectionSizeObserver(Collection *collection) : collection(collection) {
-    collection->addObserver(this);
-}
+#include <iostream>
 
-CollectionSizeObserver::~CollectionSizeObserver() {
-    collection->removeObserver(this);
+CollectionSizeObserver::CollectionSizeObserver(Collection* collection) : collection(collection) {
+    collection->addObserver(this);
 }
 
 void CollectionSizeObserver::update() {
     lastSize = collection->getSize();
-}
-
-int CollectionSizeObserver::getLastSize() const {
-    return lastSize;
+    std::cout << "\n\033[1;33;40m[OBSERVER MESSAGE] Notes in the collection |" << collection->getName() << "| --> " << lastSize << "\033[0m" << std::endl;
 }
